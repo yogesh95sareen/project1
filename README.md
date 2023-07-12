1e06a961d0aac81a629da6f8f6d0d0aba8baf9d6
savi2009 <savita.ak2009@gmail.com>
2023-07-12 19:22:12 +0530

    Sample Project created

---
+++ README.md
@@ -0,0 +1,2 @@
+# devops
+Learn Devops
---
+++ src/main/java/com/cloudsmart/App.java
@@ -0,0 +1,14 @@
+package com.cloudsmart;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+		int i;
+
+		for(i=0;i<=300;i++)
+		{
+			i += 4;
+   		}
+	 }
+}
